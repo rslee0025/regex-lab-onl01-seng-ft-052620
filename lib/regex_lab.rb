130cb37d989097b611411643090df33d39738976
Rachel S. Lee <rslee0025@gmail.com>
2020-06-10 00:33:39 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,29 @@
 def starts_with_a_vowel?(word)
-
+   if word.match(/^[aeiouAEIOU]/)
+    true
+  else
+   false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/un\w+ing/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
 
+   if text.match(/^[A-Z].+[[:punct:]]$/)
+   true
+ else
+   false
+  end
 end
 
 def valid_phone_number?(phone)
 
+  phone.scan(/\b\(?\d{3}\)?\s?-?\d{3}-?\s?\d{4}\b/).empty? ? false : true
 end
